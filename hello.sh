c1e0159a6ff7af932b1adfd12e87bfec4571c668
Iamneo <techadmin@iamneo.ai>
2023-09-15 22:09:51 +0000

    Backup Commit

--- hello.sh
+++ hello.sh
@@ -1,1 +1,11 @@
-#!/bin/
+#!/bin/bash
+
+read -p "Enter the File " f_name
+
+read -p "Enter the text " text
+
+touch $f_name
+
+echo $text > $f_name
+
+echo "The file $f_name has been created and text is $text"
