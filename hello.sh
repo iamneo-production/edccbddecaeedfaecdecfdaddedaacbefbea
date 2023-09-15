d5040aae0427c079baefa5f32a903b7bf3258672
Iamneo <techadmin@iamneo.ai>
2023-09-15 20:03:40 +0000

    Backup Commit

---
+++ hello.sh
@@ -0,0 +1,9 @@
+read -p "enter number " number
+echo
+if [ "$number" > "6" ]; then
+echo "number is greater than 6"
+else
+echo "number is lesser than 6"
+fi;
+
+
---
+++ texting.sh
@@ -0,0 +1,7 @@
+read -p "enter word " word 
+echo 
+if [ $word = "hello" ]; then
+echo "word is correct"
+else
+echo "incorrect word"
+fi;
