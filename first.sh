7389951a20a926ddc47900720de74b538e937086
Iamneo <techadmin@iamneo.ai>
2023-09-16 03:47:51 +0000

    Backup Commit

--- first.sh
+++ first.sh
@@ -1,16 +1,12 @@
 
-#!bin/bash 
-
-read -p "Enter Folder " f_name
-read -p "Enter File " fi_name
-
-# mkdir ~/$f_name
-
-# touch ~/$f_name/$fi_name
-
-# echo "The folder $f_name and the file $fi_name have been created."
-
+#!/bin/bash 
 
 cd /home/coder
 
-mkdir 
+mkdir f_name
+
+touch fi_name
+
+cd /home/coder/project/workspace
+
+echo "The folder $f_name and the file $fi_name have been created."
